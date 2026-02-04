ffc1ba18fce0834687da9936601a9f3524b56835
Juan Pena <juan.sebastian.pena.2000@gmail.com>
2026-02-04 11:26:29 -0500

    Mac OS

---
+++ MUI-Maps/MUI-Maps/MUI_MapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MUI_MapsApp.swift
+//  MUI-Maps
+//
+//  Created by Juan Pena on 2026-02-04.
+//
+
+import SwiftUI
+
+@main
+struct MUI_MapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MUI-Maps/MUI-MapsTests/MUI_MapsTests.swift
@@ -0,0 +1,17 @@
+//
+//  MUI_MapsTests.swift
+//  MUI-MapsTests
+//
+//  Created by Juan Pena on 2026-02-04.
+//
+
+import Testing
+@testable import MUI_Maps
+
+struct MUI_MapsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
